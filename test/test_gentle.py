2ef50884e4cb729e7ab7d47c091bed8c73982235
andi-ra <adulla95@gmail.com>
2019-11-04 21:55:26 +0100

    Modified test to take out risky command
    Signed-off-by: andi <hw@rebellaince.it>

--- test/test_gentle.py
+++ test/test_gentle.py
@@ -44,9 +44,10 @@ class TestGentle(TestCase):
     def test___init__(self):
         self.assertRaises(AttributeError, Gentle, config, interface="eth0")
 
-    def test_make_command(self):
-        obj = Gentle(config, interface)
-        self.assertEqual(obj.make_command(), str(0))
+    # def test_make_command(self):
+    #       Tanto questo già passava....
+    #     obj = Gentle(config, interface)
+    #     self.assertEqual(obj.make_command(), str(0))
 
     def test_reset_old_config(self):
         """This test is idempotent I can run it how many times i want it
